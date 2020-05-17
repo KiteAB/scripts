10c682db5ccf2eee406d17e48a7ceddea1053c93
KiteAB <kiteabpl@outlook>
2020-05-17 21:14:24 +0800

    updated scripts

--- git.sh
+++ git.sh
@@ -3,3 +3,24 @@
 echo "Starting commit scripts, dwm, st and nvim..."
 echo "Commiting scripts..."
 cd ~/scripts && git status && git add . && git commit -m "updated scripts" && git push --set-upstream origin master
+echo "Commited scripts."
+
+sleep 2
+
+echo "Commiting dwm..."
+cd ~/dwm && git status && git add . && git commit -m "updated dwm" && git push --set-upstream origin master
+echo "Commited dwm."
+
+sleep 2
+
+echo "Commiting st..."
+cd ~/st && git status && git add . && git commit -m "updated st" && git push --set-upstream origin master
+echo "Commited st."
+
+sleep 2
+
+echo "Commiting nvim..."
+cd ~/.config/nvim && git status && git add . && git commit -m "updated nvim config" && git push --set-upstream origin master
+echo "Commited nvim."
+
+echo "Commit work are done."
