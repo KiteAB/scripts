a1ad9319a47a496e3fe764eb9b3cf06b0789af7a
KiteAB <kiteabpl@outlook.com>
2020-06-03 18:40:35 +0800

    picom

--- autostart.sh
+++ autostart.sh
@@ -2,10 +2,10 @@
 
 /bin/bash ~/scripts/dwm-status.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
+picom -b
 nm-applet &
 xfce4-power-manager &
 fcitx &
