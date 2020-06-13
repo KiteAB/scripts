5772ff5c554f31fd11f7ec4fa41076dd0333d417
KiteAB <kiteabpl@outlook.com>
2020-06-13 09:36:54 +0800

    code>qwerty game>colemak

--- setxmodmap-code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f "~/.xmodmap-code" ]
-then
-	xmodmap ~/.xmodmap-code
-else
-	echo "xmodmap file not found."
-fi
--- setxmodmap-game.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f "~/.xmodmap-game" ]
-then
-	xmodmap ~/.xmodmap-game
-else
-	echo "xmodmap file not found."
-fi
