641dadab3ed6dfb13cce9ab198b2e14960d049b6
David Zhang <kiteabpl@outlook.com>
2020-07-02 15:52:18 +0800

    setxkbmap

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,7 @@
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
+/bin/bash ~/scripts/setxmodmap-colemak.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
 nm-applet &
