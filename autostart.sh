5f1f0afe28f5b07ba65a871ef5bb5252c38dc5fa
David Zhang <kiteabpl@outlook.com>
2020-07-29 16:59:32 +0800

    added conky autostart

--- autostart.sh
+++ autostart.sh
@@ -7,6 +7,7 @@
 /bin/bash ~/scripts/setxmodmap-colemak.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
+conky -d
 nm-applet &
 xfce4-power-manager &
 kdeconnect-indicator &
