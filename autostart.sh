d2cbe31ffbe672b32fd9ae50460631e695001174
David Zhang <kiteabpl@outlook.com>
2020-08-13 19:30:43 +0800

    change colemak script run's locale

--- autostart.sh
+++ autostart.sh
@@ -4,7 +4,6 @@
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
-/bin/bash ~/scripts/setxmodmap-colemak.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
 nm-applet &
@@ -13,4 +12,5 @@ kdeconnect-indicator &
 fcitx5 &
 #xfce4-volumed-pulse &
 /bin/bash ~/scripts/run-mailsync.sh &
+/bin/bash ~/scripts/setxmodmap-colemak.sh &
 ~/scripts/autostart_wait.sh &
