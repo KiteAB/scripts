724c1e8e52a9fc59bc1ff48cf6e4f72eec0e4f70
David Zhang <kiteabpl@outlook.com>
2020-08-14 21:08:40 +0800

    swap caps and lctrl

--- setxmodmap-colemak.sh
+++ setxmodmap-colemak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-setxkbmap us colemak -option -option caps:swapescape -option ctrl:swap_lalt_lctl -option lv3:ralt_alt
+setxkbmap us colemak -option -option ctrl:swapcaps -option lv3:ralt_alt
 xset r rate 250 30
 
