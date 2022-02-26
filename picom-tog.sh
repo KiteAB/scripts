25b462474943469710db226dbc962904c6ced1d8
KiteAB <kiteabpl@outlook.com>
2022-02-26 19:06:29 +0800

    stable toggle script for picom

--- picom-tog.sh
+++ picom-tog.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-result=$(ps ax|grep -v grep|grep picom)
-if [ "$result" == "" ]; then
+TOGGLE=/tmp/.toggle
+
+if [ ! -e $TOGGLE ]; then
+  touch $TOGGLE
   eval "picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b"
 else
+  rm $TOGGLE
   eval "killall picom"
 fi
