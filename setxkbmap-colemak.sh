852a6cd8b8e47c8899fd5ad875cdbe75d1f43db8
K1t3AB <kiteab233@proton.me>
2022-08-05 18:21:47 +0800

    fix

--- setxkbmap-colemak.sh
+++ setxkbmap-colemak.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 setxkbmap us colemak -option -option ctrl:swapcaps -option lv3:ralt_alt
-sleep 10 # to prevent failure of next line
+xset r rate 250 30 # to launch script manually
+sleep 5 # to prevent failure of next line when boot
 xset r rate 250 30
